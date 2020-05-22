b1d2ad33075fad3bca4886f17925d4d8740cc062
Matthew Gordon <matt@beelzebub.lan>
2020-05-22 15:58:54 -0700

    Fixed bug

--- switch_tree/main.py
+++ switch_tree/main.py
@@ -28,20 +28,23 @@ def get_switch(dev, debounce=DEBOUNCE_NUM):
 		time.sleep(DEBOUNCE_SLEEP)
 	return True
 
-def ramp_reverse(ramp_time=50, ramp_segments=6):
+def ramp_reverse(ramp_time=50, ramp_segments=10):
 	# ramp_time is in ms
 	pwm_val = MOTOR_SPEED
 	initial_motor_dir = motor_dir.value
 	delay_time = ramp_time/ramp_segments/1000
+	delta = 2*MOTOR_SPEED/ramp_segments
 	for i in range(ramp_segments):
-		pwm_val = int(pwm_val - (MOTOR_SPEED/ramp_segments))
+		print(i)
+		pwm_val = int(pwm_val - delta)
 		if pwm_val < 0:
 			motor_speed = -pwm_val
 			motor_dir.value = (not initial_motor_dir)
 		else:
 			motor_speed = pwm_val
 		pwm.duty_cycle = motor_speed
-		time.delay(delay_time)
+		time.sleep(delay_time)
+	pwm.duty_cycle = MOTOR_SPEED
 
 
 #limit_far = DigitalInOut(board.D2)
