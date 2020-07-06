470f2e9e7f5a73adaceed293827172f4ee7e6587
Matthew Gordon <inverarity@gmail.com>
2020-07-05 21:46:17 -0700

    Added a reverse sensor

--- pendulum_1/basher/basher.ino
+++ pendulum_1/basher/basher.ino
@@ -1,10 +1,12 @@
 
 #define FWD HIGH
 #define BWD LOW
-#define PWM_VAL 255
-const int sensorPin = 2;
+#define PWM_VAL 85
+const int sensorFwd = 8;
+const int sensorRev = 6;
 const int pwm =  3;
 const int dir = 11;
+const int led = 13;
 const int RAND_PERIOD = 5; // 10 seconds
 unsigned long t0;
 unsigned long time_delta;
@@ -14,27 +16,28 @@ void setup() {
   pinMode(pwm, OUTPUT);
   analogWrite(pwm, 0);
   pinMode(dir, OUTPUT);
-  pinMode(sensorPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(sensorFwd, INPUT);
+  pinMode(sensorRev, INPUT);
   randomSeed(analogRead(0));
+  digitalWrite(led, LOW);
 }
 
 void loop() {
   if (random(0, RAND_PERIOD) == 0) {
-    // forward
-    analogWrite(pwm, PWM_VAL);
-    digitalWrite(dir, FWD);
-    t0 = millis();
-    while(digitalRead(sensorPin) == HIGH) {
-      delay(1); // wait for the limit switch
-    }
-    time_delta = millis() - t0;
-    t0 = millis();
-    digitalWrite(dir, BWD); // reverse for the same amount of time we went forward
-    while(millis() - t0 < time_delta) {
-      delay(10);
-    }
-    analogWrite(pwm, 0);
+      analogWrite(pwm, PWM_VAL);
+      digitalWrite(dir, FWD);
+      digitalWrite(led, HIGH);
+      while(digitalRead(sensorFwd) == HIGH) {
+      }
+      digitalWrite(led, LOW);
+      delay(50);
+      digitalWrite(dir, BWD); // reverse for the same amount of time we went forward
+      while(digitalRead(sensorRev) == HIGH) {
+      }
+      analogWrite(pwm, 0);
   }
   delay(1000);
-
-}
+ }
+  
