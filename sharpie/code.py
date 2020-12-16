717b414aa65b7d23e98bf5ee749c2e79ff1309e8
Matthew Gordon <matt@beelzebub.lan>
2020-12-15 21:57:39 -0800

    cp

--- sharpie/code.py
+++ sharpie/code.py
@@ -1,18 +1,54 @@
 import time
+import adafruit_dotstar
 import board
+from digitalio import Direction, DigitalInOut, Pull
 from adafruit_mcp230xx.mcp23017 import MCP23017
-from digitalio import Direction
-i2c = board.I2C()
-mcp = MCP23017(i2c, address=0x20)
-while not i2c.try_lock():
-    pass
+import busio
+import touchio
 
-try:
-    while True:
-        print("I2C addresses found:", [hex(device_address)
-              for device_address in i2c.scan()])
-        time.sleep(2)
+touch_pad = board.A4
+touch = touchio.TouchIn(touch_pad)
+I2C_ADDR = 0x20
 
-finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
-    i2c.unlock()
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+COLORS = [GREEN, BLUE, YELLOW, RED]
+
+
+
+
+led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
+led[0] = RED
+
+i2c = busio.I2C(board.SCL, board.SDA)
+mcp = MCP23017(i2c, address=I2C_ADDR)
+KEYS = {'2': 0, '0': 1, 'P': 2, 'C': 3}
+
+def keypress(k, duration = 0.25):
+	p = mcp.get_pin(KEYS[str(k)])
+	p.value = True
+	time.sleep(duration)
+	p.value = False
+
+
+for n in range(16):
+	pin = mcp.get_pin(n)
+	pin.direction = Direction.OUTPUT
+	pin.value = False
+
+
+led[0]= RED
+n = 0
+while(True):
+	if touch.value:
+		print(n)
+		mcp.get_pin(n).value = True
+		mcp.get_pin((n - 1)%16).value = False
+		led[0] = COLORS[n % 4]
+		time.sleep(0.5)
+		n+= 1
+
 
